d96d50c442bab55b4752ca8c01aae2f70e1f2a7c
Aminatakabba <aminata-kabba@hotmail.com>
2020-09-04 13:06:44 +0100

    Updates

---
+++ precache-manifest.ba1204fe4cdf8605745b7a2ce94c3b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcaf64073f3975ab06d288f95b3dfbe",
+    "url": "/recipe_generator_reactapp/index.html"
+  },
+  {
+    "revision": "542cc2a172bc94c3d769",
+    "url": "/recipe_generator_reactapp/static/css/main.2f866a99.chunk.css"
+  },
+  {
+    "revision": "e6607ab21eaf90bf52fd",
+    "url": "/recipe_generator_reactapp/static/js/2.b51a9c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe_generator_reactapp/static/js/2.b51a9c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542cc2a172bc94c3d769",
+    "url": "/recipe_generator_reactapp/static/js/main.0223b117.chunk.js"
+  },
+  {
+    "revision": "bada55c8e159bf14e471",
+    "url": "/recipe_generator_reactapp/static/js/runtime-main.7d76ab57.js"
+  }
+]);
